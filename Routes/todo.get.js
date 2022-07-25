ca741fd392dcfe91e07b3508960a8c47caba631c
TemaLopin <artyomlopin@gmail.com>
2022-07-25 17:27:30 +0300

    create server

---
+++ Routes/todo.get.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs").promises;
+
+router.get("/todos", (req, res) => {
+  fs.readFile("todoList.json").then((date) => {
+    let todos = JSON.parse(date);
+    if (req.query.filterBy === "done") {
+      todos = todos.filter((item) => item.done);
+    }
+    if (req.query.filterBy === "undone") {
+      todos = todos.filter((item) => !item.done);
+    }
+
+    if (req.query.order === "asc") {
+      todos = todos.sort((a, b) => {
+        return new Date(a.createdAt) - new Date(b.createdAt);
+      });
+    }
+    if (req.query.order === "desc") {
+      todos = todos.sort((a, b) => {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      });
+    }
+
+    if (req.query.pp && req.query.page) {
+      const lastTaskIndex = req.query.page * req.query.pp;
+      const firstTaskIndex = lastTaskIndex - req.query.pp;
+      todos = todos.slice(firstTaskIndex, lastTaskIndex);
+    }
+
+    res.send({ count: todos.length, todos });
+  });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use("/", require('./Routes/todo.get'))
+
+app.listen(3000)
